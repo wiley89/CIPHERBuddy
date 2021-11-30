09b3e7f230c1b66fcffa91145bf8fdadc3511380
isabellarl573 <isabellarl573@gmail.com>
2021-11-29 23:05:15 -0500

    vigenere gui

--- src/VigenerePage.java
+++ src/VigenerePage.java
@@ -12,43 +12,43 @@ public class VigenerePage {
         frame.setTitle("CipherBuddy");
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        JPanel headingPanel = new JPanel();
-        JLabel headingLabel = new JLabel("Vigenere Cipher");
-        headingPanel.add(headingLabel);
+        JPanel headerPanel = new JPanel();
+        JLabel headerLabel = new JLabel("Vigenere Cipher");
+        headerPanel.add(headerLabel);
         JPanel panel = new JPanel(new GridBagLayout());
-// Constraints for the layout
         GridBagConstraints constr = new GridBagConstraints();
-        constr.insets = new Insets(5, 5, 5, 5);
-        constr.anchor = GridBagConstraints.WEST;
-// Setting initial grid values to 0,0
-        constr.gridx=0;
-        constr.gridy=0;
-        JLabel keyLabel = new JLabel("Enter your key:");
-        JLabel inputLabel = new JLabel("Input:");
+        //constr.insets = new Insets(5, 5, 5, 5);
+        //constr.anchor = GridBagConstraints.WEST;
 
-        JLabel encryptedLabel = new JLabel("");
-        JLabel decryptedLabel = new JLabel("");
-        JLabel resultLabel = new JLabel("");
+        constr.gridx=0; constr.gridy=0;
+        JLabel keyLabel = new JLabel("Enter your key:");
+        JLabel processLabel = new JLabel("");
+
+        JLabel cipherLabel = new JLabel("Vigenere Cipher:");
+        JLabel explanationLabel1 = new JLabel("The vigenere cipher uses an polyalphabetic  key, ");
+        JLabel explanationLabel2 = new JLabel("it ciphers each character with the caesar cipher");
+        JLabel explanationLabel3 = new JLabel("of the corresponding key character");
 
         JTextField keyInput = new JTextField(20);
-        JTextArea inputArea = new JTextArea(5, 20);
+        //JTextArea inputArea = new JTextArea(5, 20);
 
 
         constr.gridx=0; constr.gridy=1;
-        panel.add(encryptedLabel, constr);
-        panel.add(decryptedLabel, constr);
-        constr.gridx=1;
-        panel.add(resultLabel, constr);
+        panel.add(cipherLabel, constr);
         constr.gridx=0; constr.gridy=2;
         panel.add(keyLabel, constr);
         constr.gridx=1;
         panel.add(keyInput, constr);
         constr.gridx=0; constr.gridy=3;
-        panel.add(inputLabel, constr);
+        panel.add(processLabel, constr);
         constr.gridx=1;
-        panel.add(inputArea, constr);
-        constr.gridx=0; constr.gridy=4;
-        constr.gridwidth = 2;
+        panel.add(explanationLabel1, constr);
+        constr.gridy=4;
+        panel.add(explanationLabel2, constr);
+        constr.gridy=5;
+        panel.add(explanationLabel3, constr);
+        constr.gridy=6;
+        //constr.gridwidth = 1;
         constr.anchor = GridBagConstraints.CENTER;
         //encrypt button
         JButton encryptButton = new JButton("Encrypt");
@@ -57,38 +57,21 @@ public class VigenerePage {
         {
             public void actionPerformed(ActionEvent e)
             {
-                encryptedLabel.setText("Encrypted input:");
-                decryptedLabel.setText("");
+                //encryptedLabel.setText("Encrypted input:");
+                //decryptedLabel.setText("");
+                /*
                 try {
-                    resultLabel.setText(VigenereCipher.encryptByteArray(inputArea.getText().getBytes(), keyInput.getText()).toString());
+                    //resultLabel.setText(VigenereCipher.encryptByteArray(inputArea.getText().getBytes(), keyInput.getText()).toString());
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
-                }
+                }*/
 
                 keyInput.setText("");
-                inputArea.setText("");
             }
         });
         panel.add(encryptButton, constr);
-        constr.gridx=2; constr.gridy=4;
-        JButton decryptButton = new JButton("Decrypt");
-        panel.add(decryptButton, constr);
-        // add a listener to button
-        decryptButton.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                encryptedLabel.setText("");
-                decryptedLabel.setText("Decrypted input:");
-                resultLabel.setText(VigenereCipher.decryptByteArray(inputArea.getText().getBytes(), keyInput.getText()).toString());
 
-
-                keyInput.setText("");
-                inputArea.setText("");
-            }
-        });
-        panel.add(decryptButton, constr);
-        mainPanel.add(headingPanel);
+        mainPanel.add(headerPanel);
         mainPanel.add(panel);
         frame.add(mainPanel);
         frame.pack();
