83ee275f0d42ed0ccd9c85e4ad49b20f3de64c7d
meSeeks7 <67931270+meSeeks7@users.noreply.github.com>
2021-11-30 02:49:32 -0500

    Updated homePage
    Connected Vigenere Cipher to homepage

--- src/Homepage.java
+++ src/Homepage.java
@@ -1,34 +1,177 @@
-import java.awt.BorderLayout;
+
+/**
+ * Write a description of class homepage here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import java.util.Scanner;
 
-public class Homepage {
-    private JFrame frame;
-    private JPanel pane;
-    private JButton button1;
-    private JButton button2;
+import java.awt.dnd.DropTarget;
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Homepage()::createAndShowGui);
+public class homePage extends JFrame
+{
+    public static void main(String[] args)
+    {
+        homePage homepage = new homePage();
     }
 
-    public void createAndShowGui() {
-        frame = new JFrame(getClass().getSimpleName());
+    public homePage()
+    {
+        // home page
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        GridLayout homelayout = new GridLayout(3, 1);
+        setLayout(homelayout);
+        setTitle("CSDS 344 Team 10 Encryption/ Decryption Algorithms");
+        setSize(400, 400);
+        
+        JLabel logo = new JLabel(new ImageIcon("logo.png"));
+        Button b1 = new Button("Encrypt");
+        Button b2 = new Button("Decrypt");
 
-        pane = new JPanel();
-        pane.setLayout(new BoxLayout(pane, BoxLayout.LINE_AXIS));
+        add(logo);
+        add(b1);
+        add(b2);
 
-        button1 = new JButton("Encryption");
-        button2 = new JButton("Decryption");
+        Scanner input = new Scanner(System.in);
+        b1.addActionListener(new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent e) 
+                {
+                    if (e.getSource() == b1)
+                    {
+                        System.out.println("pressed encrypt");
+                        setVisible(true);
+                        remove(logo);
+                        remove(b1);
+                        remove(b2);
 
-        pane.add(button1);
-        pane.add(new JLabel(new ImageIcon("logo.png")));
-        pane.add(button2);
+                        GridLayout filelayout = new GridLayout(3, 1);
+                        setLayout(filelayout);
+                        setSize(400, 400);
 
-        frame.add(pane, BorderLayout.SOUTH);
-        //frame.add(new JLabel(new ImageIcon("logo.png")));
-        frame.pack();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        Button file = new Button("Drag and Drop a File to Encrypt!");
+                        Button submit = new Button("Click here once you have uploaded your file.");
+
+                        add(file);
+                        add(submit);
+
+                        MyDragDropListener inputFile = new MyDragDropListener();
+                        new DropTarget(file, inputFile); 
+                        submit.addActionListener(new ActionListener() 
+                            {
+                                public void actionPerformed(ActionEvent e) 
+                                {
+                                    if(e.getSource()== submit)
+                                    {
+                                        setVisible(true);
+                                        remove(file);
+                                        remove(submit);
+
+                                        GridLayout encryptlayout = new GridLayout(2, 2);
+                                        setLayout(encryptlayout);
+                                        setSize(400, 400);
+
+                                        Button eb1 = new Button("Vigenere Cipher");
+                                        Button eb2 = new Button("Encryption Algorithm 2");
+                                        Button eb3 = new Button("Encryption Algorithm 3");
+                                        Button eb4 = new Button("Encryption Algorithm 4");
+
+                                        add(eb1);
+                                        add(eb2);
+                                        add(eb3);
+                                        add(eb4);
+
+                                        eb1.addActionListener(new ActionListener() 
+                                            {
+                                                public void actionPerformed(ActionEvent e) 
+                                                {
+                                                    if(e.getSource()== eb1)
+                                                    {
+                                                        System.out.println("pressed vig cypher");
+                                                        VigenereEncrypt vig = new VigenereEncrypt();
+                                                        vig.vig();
+                                                    }
+                                                }
+                                            });
+                                    }
+                                }
+                            });
+                    }
+                }
+            });
+
+        b2.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (e.getSource() == b2){
+                        System.out.println("pressed decrypt");
+                        setVisible(true);
+                        remove(logo);
+                        remove(b1);
+                        remove(b2);
+
+                        GridLayout filelayout = new GridLayout(2, 1);
+                        setLayout(filelayout);
+                        setSize(400, 400);
+
+                        Button file = new Button("Drag and Drop a File to Decrypt!");
+                        Button submit = new Button("Click here once you have uploaded your file.");
+
+                        add(file);
+                        add(submit);
+
+                        MyDragDropListener inputFile = new MyDragDropListener();
+                        new DropTarget(file, inputFile); 
+                        submit.addActionListener(new ActionListener() 
+                            {
+                                public void actionPerformed(ActionEvent e) 
+                                {
+                                    if(e.getSource()==submit)
+                                    {
+                                        setVisible(true);
+                                        remove(file);
+                                        remove(submit);
+
+                                        GridLayout encryptlayout = new GridLayout(2, 2);
+                                        setLayout(encryptlayout);
+                                        setSize(400, 400);
+
+                                        Button db1 = new Button("Vigenere Cipher");
+                                        Button db2 = new Button("Decryption Algorithm 2");
+                                        Button db3 = new Button("Decryption Algorithm 3");
+                                        Button db4 = new Button("Decryption Algorithm 4");
+
+                                        add(db1);
+                                        add(db2);
+                                        add(db3);
+                                        add(db4);
+                                        
+                                        db1.addActionListener(new ActionListener() 
+                                            {
+                                                public void actionPerformed(ActionEvent e) 
+                                                {
+                                                    if(e.getSource()== db1)
+                                                    {
+                                                        System.out.println("pressed vig cypher");
+                                                        VigenereDecrypt vig = new VigenereDecrypt();
+                                                        vig.vig();
+                                                    }
+                                                }
+                                            });
+                                    }
+                                }
+                            });
+                    }
+                }
+            });
     }
 }
---
+++ src/VigenereDecrypt.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.io.IOException;
+
+public class VigenereDecrypt 
+{
+    /**
+     * NEED to update method to take a byte array input in the method header
+     */
+    public void vig()
+    {
+        // Create frame with title
+        JFrame frame= new JFrame();
+        frame.setTitle("CipherBuddy");
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        JPanel headerPanel = new JPanel();
+        JLabel headerLabel = new JLabel("Vigenere Cipher");
+        headerPanel.add(headerLabel);
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints constr = new GridBagConstraints();
+        //constr.insets = new Insets(5, 5, 5, 5);
+        //constr.anchor = GridBagConstraints.WEST;
+
+        constr.gridx=0; constr.gridy=0;
+        JLabel keyLabel = new JLabel("Enter your key:");
+        JLabel processLabel = new JLabel("");
+
+        JLabel cipherLabel = new JLabel("Vigenere Cipher:");
+        JLabel explanationLabel1 = new JLabel("The vigenere cipher uses an polyalphabetic  key, ");
+        JLabel explanationLabel2 = new JLabel("it ciphers each character with the caesar cipher");
+        JLabel explanationLabel3 = new JLabel("of the corresponding key character");
+
+        JTextField keyInput = new JTextField(20);
+        //JTextArea inputArea = new JTextArea(5, 20);
+
+        constr.gridx=0; constr.gridy=1;
+        panel.add(cipherLabel, constr);
+        constr.gridx=0; constr.gridy=2;
+        panel.add(keyLabel, constr);
+        constr.gridx=1;
+        panel.add(keyInput, constr);
+        constr.gridx=0; constr.gridy=3;
+        panel.add(processLabel, constr);
+        constr.gridx=1;
+        panel.add(explanationLabel1, constr);
+        constr.gridy=4;
+        panel.add(explanationLabel2, constr);
+        constr.gridy=5;
+        panel.add(explanationLabel3, constr);
+        constr.gridy=6;
+        //constr.gridwidth = 1;
+        constr.anchor = GridBagConstraints.CENTER;
+        //encrypt button
+        JButton decryptButton = new JButton("Decrypt");
+
+        decryptButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    //encryptedLabel.setText("Encrypted input:");
+                    //decryptedLabel.setText("");
+                    /*
+                    try {
+                    //resultLabel.setText(VigenereCipher.decryptByteArray(inputArea.getText().getBytes(), keyInput.getText()).toString());
+                    } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                    }*/
+
+                    keyInput.setText("");
+                }
+            });
+        panel.add(decryptButton, constr);
+
+        mainPanel.add(headerPanel);
+        mainPanel.add(panel);
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
